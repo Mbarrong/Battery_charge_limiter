3db505bb726aedeb220b311aa144b0860a6a89ea
michael <mbarrong@yahoo.com>
2022-05-13 16:13:57 -0700

    Added state persistence. The last charge limit is saved and reloaded the next time the program is run. It crates a file in the user's home directory. This file is owned by root and is named Battery_charge_limiter.store.

--- Battery_charge_limiter.py
+++ Battery_charge_limiter.py
@@ -5,6 +5,10 @@ import os
 import sys
 import subprocess
 
+UPPER_LIMIT = 100   # The highest percentage you want the battery to stop charging
+LOWER_LIMIT = 5     # The lowest percentage you want the battery to stop charging
+PERSIST_FILE_NAME = "Battery_charge_limiter.store"
+
 # Creating a new window and configurations
 window = tkinter.Tk()
 window.title("ASUS Battery Utility")
@@ -17,23 +21,49 @@ if os.geteuid() != 0:
     sys.exit()
 
 
-def set_charge_limit():
+def set_saved_charge_limit():
+    try:
+        with open(PERSIST_FILE_NAME, mode="r") as persist_file:
+            init_percentage = persist_file.read()
+    except:
+        pass
+    else:
+        try:
+            init_percentage = int(init_percentage)
+        except ValueError:
+            with open(PERSIST_FILE_NAME, mode="w") as erase_persist_file:
+                pass
+        else:
+            if init_percentage > UPPER_LIMIT:
+                init_percentage = UPPER_LIMIT
+            if init_percentage < LOWER_LIMIT:
+                init_percentage = LOWER_LIMIT
 
+            entry1.delete(0, "end")
+            entry1.insert(0, str(init_percentage))
+            set_charge_limit()
+
+
+def set_charge_limit():
     try:
         percentage = int(entry1.get())
     except ValueError:
         entry1.delete(0, "end")
         entry1.config(background="white")
     else:
-        if percentage > 100:
-            percentage = 100
-        if percentage < 5:
-            percentage = 5
+        if percentage > UPPER_LIMIT:
+            percentage = UPPER_LIMIT
+        if percentage < LOWER_LIMIT:
+            percentage = LOWER_LIMIT
         entry1.delete(0, "end")
         entry1.insert(0, str(percentage))
         with open("/sys/class/power_supply/BAT0/charge_control_end_threshold", mode="w") as bat_file:
             bat_file.write(str(percentage))
         refresh()
+
+        with open(PERSIST_FILE_NAME, mode="w") as save_file:
+            save_file.write(str(percentage))
+
 
 
 def return_key_pressed(event):
@@ -61,6 +91,8 @@ entry1.focus_set()
 entry1.grid(column=1, row=2)
 
 refresh()
+set_saved_charge_limit()
+
 window.bind('<Return>', return_key_pressed)
 
 
